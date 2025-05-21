6c97f6db3774aa77e65cd9adbf179f05c8f6129d
KolDS0811 <fatbahad@gmail.com>
2025-05-21 22:39:05 +0300

    added base logging

--- LoggerV2.0/Logger.h
+++ LoggerV2.0/Logger.h
@@ -1,7 +1,177 @@
-#pragma once
+﻿#pragma once
+#include <Windows.h>
 #include <array>
+#include <concepts>
+#include <cstddef>
+#include <cstdio>
+#include <memory_resource>
+#include <string>
 
-
-class Logger
+namespace Log
 {
-};
+  class Logger final
+  {
+    std::array<std::byte, 10240> Raw{};
+    std::pmr::monotonic_buffer_resource BufferResource;
+    std::pmr::vector<std::pmr::string> StringsVec;
+
+
+  public:
+    static Logger& Get()
+    {
+      static Logger instance;
+      return instance;
+    }
+
+    template <typename T>
+    void Logging(T&& arg)
+    {
+      Logs(std::forward<T>(arg));
+      PrintLog();
+      StringsVec.clear();
+    }
+
+    template <typename T, typename... Types>
+    void Logging(T&& arg, Types&&... args)
+    {
+      Logs(std::forward<T>(arg));
+      Logging(std::forward<Types>(args)...);
+    }
+
+
+  private:
+    Logger(const Logger&) = delete;
+    Logger(const Logger&&) = delete;
+    Logger& operator=(const Logger&) = delete;
+    Logger& operator=(const Logger&&) = delete;
+
+    inline void PrintLog()
+    {
+      for (const auto& s : StringsVec)
+      {
+        OutputDebugString(s.c_str());
+      }
+      OutputDebugString("\n");
+    }
+
+    Logger() : BufferResource{Raw.data(), Raw.size(), std::pmr::get_default_resource()}, StringsVec{&BufferResource} {}
+
+    inline void Logs(const char* arg)
+    {
+      if (!arg)
+      {
+        StringsVec.emplace_back(" Is nullptr");
+        return;
+      }
+      StringsVec.emplace_back(arg);
+    }
+
+    template <typename T>
+      requires std::is_floating_point_v<T> || std::is_integral_v<T>
+    void Logs(T arg)
+    {
+      static constexpr size_t BufferSize = 64;
+      char thread_local buffer[BufferSize];
+
+      if constexpr (std::is_integral_v<T>)
+      {
+        snprintf(buffer, BufferSize, "%d", static_cast<int>(arg));
+      }
+      else if constexpr (std::is_floating_point_v<T>)
+      {
+        snprintf(buffer, BufferSize, "%.6f", static_cast<double>(arg));
+      }
+
+      StringsVec.emplace_back(buffer);
+    }
+
+    template <typename T>
+      requires std::is_pointer_v<T> && (!std::is_same_v<T, const char*>)
+    void Logs(T arg)
+    {
+      static constexpr size_t BufferSize = 64;
+      char thread_local buffer[BufferSize];
+
+      if (arg)
+      {
+        snprintf(buffer, BufferSize, " Address 0x%zx", reinterpret_cast<uintptr_t>(arg));
+        StringsVec.emplace_back(buffer);
+      }
+      else
+      {
+        StringsVec.emplace_back(" is nullptr ");
+      }
+    }
+
+    template <typename T>
+      requires std::is_same_v<std::decay_t<T>, std::nullptr_t>
+    void Logs(T arg)
+    {
+      StringsVec.emplace_back(" is nullptr ");
+    }
+
+    inline void Logs(std::string&& arg) { StringsVec.emplace_back(arg); }
+
+    inline void Logs(std::string& arg) { StringsVec.emplace_back(arg); }
+
+    inline void Logs() { StringsVec.emplace_back(" "); }
+
+    template <>
+    void Logs<bool>(bool arg)
+    {
+      arg ? StringsVec.emplace_back(" TRUE ") : StringsVec.emplace_back(" FALSE ");
+    }
+
+
+    template <>
+    void Logs<const wchar_t*>(const wchar_t* arg)
+    {
+      static constexpr size_t BufferSize = 256;
+      char thread_local buffer[BufferSize];
+
+      if (!arg)
+      {
+        StringsVec.emplace_back("Is nullptr");
+      }
+
+      int size = WideCharToMultiByte(CP_UTF8, 0, arg, -1, nullptr, 0, nullptr, nullptr);
+      if (size == 0 || size > static_cast<int>(BufferSize))
+      {
+        StringsVec.emplace_back(" Conversion error or too long ");
+      }
+      WideCharToMultiByte(CP_UTF8, 0, arg, -1, buffer, size, nullptr, nullptr);
+
+      StringsVec.emplace_back(buffer);
+    }
+
+    inline void Logs(std::wstring& arg) { Logs(arg.c_str()); }
+
+    template <>
+    void Logs<wchar_t>(wchar_t arg)
+    {
+      Logs(&arg);
+    }
+
+    template <>
+    void Logs<void*>(void* arg)
+    {
+      StringsVec.emplace_back(" IS VOID PTR ");
+    }
+
+    template <typename Key, typename Value>
+    void Logs(const std::pair<const Key, Value>& arg)
+    {
+      Logs(arg.first);
+      Logs(": ");
+      Logs(arg.second);
+    }
+
+  };
+
+} // Logger namespace
+
+
+#define LOG_WARNING(...) Log::Logger::Get().Logging("warning ", "FILE: ", __FILE__, " LINE: ", __LINE__, __VA_ARGS__)
+#define LOG_ERROR(...) Log::Logger::Get().Logging("error ", "FILE: ", __FILE__, " LINE: ", __LINE__, __VA_ARGS__)
+#define LOG_MESSAGE(...)                                                                                               \
+  Log::Logger::Get().Logging("information ", "FILE: ", __FILE__, " LINE: ", __LINE__, __VA_ARGS__)
--- LoggerV2.0/Source.cpp
+++ LoggerV2.0/Source.cpp
@@ -1,10 +1,69 @@
+﻿#include "Logger.h"
+#include <iostream>
+#include <format>
 
 
-
+struct TestStruct
+{
+  //std::string name{"KURWA"};
+};
 
 int main()
 {
+  int a         = 7;
+  int* ptr      = &a;
+  void* ptrVoid = nullptr;
+
+  LOG_WARNING(" dwdwdw");
+
+  LOG_WARNING(" Vector X -", 1.0, " --- Is  ", true);
+  LOG_WARNING(" poiner - ", ptr, " ");
+  LOG_MESSAGE("QUAT - W ", 0.56564, " X -  ", 0.12321, " Y - ", 1, " Z - ", 0.777777777777);
+  LOG_MESSAGE("  ", ' ', "wdw", " ", nullptr, -556494949);
+  LOG_MESSAGE("Void PTR", ptrVoid, &ptrVoid, 1525555.005255555555555555);
+  LOG_MESSAGE();
+  LOG_MESSAGE("");
+  LOG_MESSAGE("Empty string test: ", "");
+
+  double d     = 3.14;
+  double* ptrD = &d;
+
+  LOG_MESSAGE(" Double pointer - ", ptrD);
+  LOG_MESSAGE(" Nullptr test - ", nullptr);
+  LOG_MESSAGE(" Void pointer - ", static_cast<void*>(ptrD));
+  LOG_MESSAGE(" Int max - ", std::numeric_limits<int>::max);
+  LOG_MESSAGE(" Int min - ", std::numeric_limits<int>::min);
+  LOG_MESSAGE(" Float epsilon - ", std::numeric_limits<float>::epsilon());
+  LOG_MESSAGE(" Double infinity - ", std::numeric_limits<double>::infinity());
+  LOG_MESSAGE(" Negative zero - ", -0.0);
+  LOG_MESSAGE(" String literal - ", "Hello, World!");
+  LOG_MESSAGE(" StdString - ", std::string("Hello, World!"));
+  LOG_MESSAGE(" Special chars - ", "Newline:\nTab:\tQuote:\"");
+  LOG_MESSAGE(" True test - ", true);
+  LOG_MESSAGE(" False test - ", false);
+  LOG_MESSAGE(" Expression test - ", 42 > 0);
+  LOG_MESSAGE(" Expression test - ", 0 == 1);
+  LOG_MESSAGE(" Char test - ", 'A');
+  LOG_MESSAGE(" Wide char test - ", L'Ω');
+  LOG_MESSAGE(" Special char test - ", '\t', " ddd");
+  LOG_MESSAGE(" Non-printable char test - ", '\x01', " END");
+
+  std::string stre("dwqdwq");
+  LOG_MESSAGE(stre);
+
+  std::unique_ptr<TestStruct> testUnique     = std::make_unique<TestStruct>();
+  std::unique_ptr<TestStruct> testUniqueNull = nullptr;
+  LOG_MESSAGE(" test unique - ", testUnique.get());
+  LOG_MESSAGE(" test unique - ", testUniqueNull.get());
+
+  LOG_ERROR(" Adress W - ", L" D:\\Logger\\Logger");
+  LOG_ERROR(" Adress  - ", " D:\\Logger\\Logger");
+
+  std::wstring path = L" D:\\boxyboi\\License";
+  LOG_MESSAGE(path);
+
+  LOG_MESSAGE(" C++20 format test - ", std::format("Formatted: {}", 42));
 
 
-	return 0;
+  return 0;
 }
